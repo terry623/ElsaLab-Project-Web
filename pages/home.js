4402f3773bf5485f8d013cc1208b98c0dcd600fb
terry623 <terry92516@gmail.com>
2018-09-12 14:34:15 +0800

    use ant design

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const HomePage = () => (
+  <Layout>
+    <p>This is the home page</p>
+  </Layout>
+);
+
+export default HomePage;
