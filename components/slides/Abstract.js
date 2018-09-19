3d548ca47ecb89585f59cf871f6e5a860a9a2fda
terry623 <terry92516@gmail.com>
2018-09-19 23:40:04 +0800

    finish basic abstract page

--- components/slides/Abstract.js
+++ components/slides/Abstract.js
@@ -1,26 +1,54 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Col, Row } from 'antd';
 import { Parallax } from 'react-spring';
 
+import { ScreenSM, ScreenXXL } from '../size';
+import { pinkColorLight } from '../color';
+
 const BackgroundImage = '/static/background_image.jpg';
-const satellite =
-  'https://awv3node-homepage.surge.sh/build/assets/satellite4.svg';
 
 const Abstract = ({ mainOffset, scroll }) => (
   <div onClick={() => scroll(0)}>
     <Parallax.Layer offset={mainOffset} speed={0}>
       <div className="back_layer" />
     </Parallax.Layer>
-    <Parallax.Layer offset={mainOffset + 0.5} speed={0.5}>
-      <img
-        src={satellite}
-        style={{ width: '15%', marginLeft: '45%' }}
-        alt="titleCard"
-      />
+    <Parallax.Layer offset={mainOffset} speed={0.5}>
+      <div className="abstract">
+        <Row className="title_block">
+          <Col span={12} offset={4}>
+            <p className="main_title">Abstract</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12} offset={4}>
+            <p className="content_text">
+              Collecting training data from the physical world is usually
+              time-consuming and even dangerous for fragile robots, and
+              therefore, recent advances in robot learning propose to use
+              simulators as the training platform. Unfortunately, the reality
+              gap between synthetic and real visual data prohibits directly
+              migrating the models trained in virtual worlds to the real world.
+              This paper proposes a modular architecture for tackling
+              simulation- to-reality problem. The architecture separates the
+              learning model into a perception module and a control policy
+              module, and uses semantic image segmentation as the meta
+              representation for relating these two modules. The perception
+              module translates RGB images to semantic image segmentations. The
+              control policy module employs deep reinforcement learning (RL)
+              methods and takes image segmentations as its inputs. Experimental
+              results show that our architecture outperforms all baseline
+              methods in the virtual and real environments, and learns faster
+              than them.
+            </p>
+          </Col>
+        </Row>
+      </div>
     </Parallax.Layer>
     <style jsx>{`
       .back_layer {
-        background: url(${BackgroundImage});
+        background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+          url(${BackgroundImage});
         background-size: cover;
         width: 100%;
         height: 100%;
@@ -28,6 +56,49 @@ const Abstract = ({ mainOffset, scroll }) => (
         top: 0;
         left: 0;
         z-index: -100;
+      }
+      .abstract {
+        color: white;
+      }
+      .main_title {
+        color: ${pinkColorLight};
+      }
+      @media (max-width: ${ScreenSM}) {
+        // FIXME: 設計要討論
+        .abstract {
+          margin-top: 5rem;
+        }
+        .main_title {
+          font-size: 3rem;
+          margin-bottom: 1rem;
+        }
+        .content_text {
+          font-size: 1.2rem;
+        }
+      }
+      @media (min-width: ${ScreenSM}) {
+        .abstract {
+          margin-top: 5rem;
+        }
+        .main_title {
+          font-size: 3rem;
+          margin-bottom: 1rem;
+        }
+        .content_text {
+          font-size: 1.2rem;
+        }
+      }
+      @media (min-width: ${ScreenXXL}) {
+        .abstract {
+          margin-top: 9rem;
+        }
+        .main_title {
+          font-size: 4rem;
+          margin-bottom: 1rem;
+        }
+        .content_text {
+          font-size: 1.5rem;
+        }
       }
     `}</style>
   </div>
