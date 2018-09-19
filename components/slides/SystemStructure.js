7dd184da548c26917b3f61bcb47afb8307b5e7ca
terry623 <terry92516@gmail.com>
2018-09-19 22:05:12 +0800

    finish system in 1920x1080

--- components/slides/SystemStructure.js
+++ components/slides/SystemStructure.js
@@ -3,45 +3,65 @@ import React from 'react';
 import { Col, Row } from 'antd';
 import { Parallax } from 'react-spring';
 
-import { pinkColorLight } from '../color';
+import { pinkColorDark, pinkColorLight } from '../color';
 
 const ProductImage = '/static/System_structure_img.jpg';
+const BackgroundImage = '/static/background_image.jpg';
 
 const SystemStructure = ({ mainOffset, scroll }) => (
   <div onClick={() => scroll(2)}>
     <Parallax.Layer offset={mainOffset} speed={1}>
       <div className="back_layer_2" />
     </Parallax.Layer>
-    <Parallax.Layer offset={mainOffset + 0.2} speed={0.5}>
-      <Row type="flex" justify="center" align="middle">
-        <Col span={8} className="system_left">
-          <Row type="flex" justify="end">
-            <div className="media_title">
-              <p className="main_title">System Structure</p>
-            </div>
+    <Parallax.Layer offset={mainOffset} speed={0.5}>
+      <Row type="flex" justify="center">
+        <Col span={8} offset={1}>
+          <Row type="flex" justify="start">
+            <Col className="system">
+              <div className="media_title">
+                <p className="title">
+                  System
+                  <br />
+                  Structure
+                </p>
+              </div>
+            </Col>
           </Row>
         </Col>
-        <Col span={16} className="system_right">
-          <img src={ProductImage} style={{ width: '80%' }} alt="titleCard" />
+        <Col span={8}>
+          <Row type="flex" justify="end">
+            <Col className="system">
+              <img className="media" src={ProductImage} alt="titleCard" />
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Parallax.Layer>
     <style jsx global>{`
-      .media_title {
-        background-color: black;
-        margin-right: 20px;
-        width: 40%;
-        height: 475px;
-      }
-      .main_title {
-        color: white;
-      }
       .back_layer_2 {
         background-color: ${pinkColorLight};
         background-size: cover;
         width: 100%;
         height: 100%;
         z-index: -100;
+      }
+      .system {
+        margin-top: 30%;
+        height: 40rem;
+      }
+      .media_title {
+        background: url(${BackgroundImage});
+        height: 100%;
+        width: 16rem;
+      }
+      .title {
+        font-size: 3rem;
+        font-style: italic;
+        padding: 2rem;
+        color: ${pinkColorDark};
+      }
+      .media {
+        height: 100%;
       }
     `}</style>
   </div>
