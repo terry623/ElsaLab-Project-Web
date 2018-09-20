2ac5686e3884c7d75679ee76f891130acbdec684
terry623 <terry92516@gmail.com>
2018-09-21 00:27:24 +0800

    change system page to styled component

--- components/slides/SystemStructure.js
+++ components/slides/SystemStructure.js
@@ -1,100 +1,90 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import { Parallax } from 'react-spring';
 
-import { ScreenSM, ScreenXXL } from '../size';
+import { media } from '../size';
 import { pinkColorDark, pinkColorLight } from '../color';
 
 const ProductImage = '/static/System_structure_img.jpg';
 const BackgroundImage = '/static/background_image.jpg';
 
+const BackGround = styled.div`
+  background-color: ${pinkColorLight};
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  z-index: -100;
+`;
+
+const SystemCol = styled(Col)`
+  height: 30rem;
+
+  ${media.desktop`
+    height: 40rem;
+  `};
+`;
+
+const MediaTitleBlock = styled.div`
+  background: url(${BackgroundImage});
+  height: 100%;
+  width: 9rem;
+
+  ${media.desktop`
+    width: 16rem;
+  `};
+`;
+
+const Title = styled.div`
+  color: ${pinkColorDark};
+  font-style: italic;
+  font-size: 1.5rem;
+  padding: 1rem;
+
+  ${media.notebook`
+    font-size: 1.5rem;
+    padding: 1rem;
+  `};
+  ${media.desktop`
+    font-size: 3rem;
+    padding: 2rem;
+  `};
+`;
+
+const Media = styled.img`
+  height: 100%;
+`;
+
 const SystemStructure = ({ mainOffset, scroll }) => (
   <div onClick={() => scroll(2)}>
     <Parallax.Layer offset={mainOffset} speed={1}>
-      <div className="back_layer_2" />
+      <BackGround />
     </Parallax.Layer>
     <Parallax.Layer offset={mainOffset + 0.18} speed={0.5}>
       <Row type="flex" justify="center">
         <Col span={8} offset={1}>
           <Row type="flex" justify="start">
-            <Col className="system">
-              <div className="media_title">
-                <p className="title">
+            <SystemCol>
+              <MediaTitleBlock>
+                <Title>
                   System
                   <br />
                   Structure
-                </p>
-              </div>
-            </Col>
+                </Title>
+              </MediaTitleBlock>
+            </SystemCol>
           </Row>
         </Col>
         <Col span={8}>
           <Row type="flex" justify="end">
-            <Col className="system">
-              <img className="media" src={ProductImage} alt="titleCard" />
-            </Col>
+            <SystemCol>
+              <Media src={ProductImage} />
+            </SystemCol>
           </Row>
         </Col>
       </Row>
     </Parallax.Layer>
-    <style jsx global>{`
-      .back_layer_2 {
-        background-color: ${pinkColorLight};
-        background-size: cover;
-        width: 100%;
-        height: 100%;
-        z-index: -100;
-      }
-      .media_title {
-        background: url(${BackgroundImage});
-        height: 100%;
-      }
-      .title {
-        font-style: italic;
-        color: ${pinkColorDark};
-      }
-      .media {
-        height: 100%;
-      }
-      @media (max-width: ${ScreenSM}) {
-        // FIXME: 設計要討論
-        .system {
-          height: 30rem;
-        }
-        .media_title {
-          width: 9rem;
-        }
-        .title {
-          font-size: 1.5rem;
-          padding: 1rem;
-        }
-      }
-      @media (min-width: ${ScreenSM}) {
-        .system {
-          height: 30rem;
-        }
-        .media_title {
-          width: 9rem;
-        }
-        .title {
-          font-size: 1.5rem;
-          padding: 1rem;
-        }
-      }
-      @media (min-width: ${ScreenXXL}) {
-        .system {
-          height: 40rem;
-        }
-        .media_title {
-          width: 16rem;
-        }
-        .title {
-          font-size: 3rem;
-          padding: 2rem;
-        }
-      }
-    `}</style>
   </div>
 );
 
