57bbab907e80eb3b42a9559e4ff11a1791eb0669
terry623 <terry92516@gmail.com>
2018-09-21 01:13:43 +0800

    change proposed and experiment pages to styled component

--- components/slides/ExperimentalResults.js
+++ components/slides/ExperimentalResults.js
@@ -1,101 +1,79 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import { Parallax } from 'react-spring';
 
-import { ScreenSM, ScreenXXL } from '../size';
+import { media } from '../size';
 import { pinkColorDark, pinkColorLight } from '../color';
 
 const BackgroundInvertImage = '/static/background_image_invert.jpg';
 
+const Background = styled.div`
+  background: url(${BackgroundInvertImage});
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  z-index: -100;
+`;
+
+const MainTitle = styled.div`
+  color: ${pinkColorDark};
+  font-style: italic;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+
+  ${media.desktop`
+    font-size: 4rem;
+    margin-bottom: 2rem;
+  `};
+`;
+
+const BlockTitle = styled.div`
+  background: rgba(0, 0, 0, 0.4);
+  background-size: cover;
+  z-index: -100;
+  color: ${pinkColorLight};
+  font-size: 2rem;
+  width: 100%;
+  height: 6rem;
+  margin: 1rem 0;
+  padding: 1.5rem;
+
+  ${media.desktop`
+    font-size: 2.5rem;
+    height: 8rem;
+    padding: 2rem;
+  `};
+`;
+
 const ExperimentalResults = ({ mainOffset, scroll }) => (
   <div onClick={() => scroll(0)}>
     <Parallax.Layer offset={mainOffset} speed={0}>
-      <div className="back_layer" />
+      <Background />
     </Parallax.Layer>
     <Parallax.Layer offset={mainOffset + 0.18} speed={0.5}>
-      <div className="proposedMethodology">
-        <Row className="title_block">
-          <Col span={15} offset={4}>
-            <p className="main_title">Experimental Results</p>
-          </Col>
-        </Row>
-        <Row className="block">
-          <Col span={15} offset={4}>
-            <div className="block_title">
-              Model Settings and Robotic Platform
-            </div>
-          </Col>
-        </Row>
-        <Row className="block">
-          <Col span={15} offset={4}>
-            <div className="block_title">Learning Curves of the Two Tasks</div>
-          </Col>
-        </Row>
-        <Row className="block">
-          <Col span={15} offset={4}>
-            <div className="block_title">
-              Comparison in the Simulated and Real Worlds
-            </div>
-          </Col>
-        </Row>
-      </div>
+      <Row>
+        <Col span={15} offset={4}>
+          <MainTitle>Experimental Results</MainTitle>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={15} offset={4}>
+          <BlockTitle>Model Settings and Robotic Platform</BlockTitle>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={15} offset={4}>
+          <BlockTitle>Learning Curves of the Two Tasks</BlockTitle>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={15} offset={4}>
+          <BlockTitle>Comparison in the Simulated and Real Worlds</BlockTitle>
+        </Col>
+      </Row>
     </Parallax.Layer>
-    <style jsx>{`
-      .back_layer {
-        background: url(${BackgroundInvertImage});
-        background-size: cover;
-        width: 100%;
-        height: 100%;
-        z-index: -100;
-      }
-      .main_title {
-        color: ${pinkColorDark};
-        font-style: italic;
-      }
-      .block_title {
-        background: rgba(0, 0, 0, 0.4);
-        background-size: cover;
-        color: ${pinkColorLight};
-        width: 100%;
-        z-index: -100;
-        margin: 1rem 0;
-      }
-      @media (max-width: ${ScreenSM}) {
-        // FIXME: 設計要討論
-        .block_title {
-          height: 6rem;
-          font-size: 2rem;
-          padding: 1.5rem;
-        }
-        .main_title {
-          font-size: 3rem;
-          margin-bottom: 1rem;
-        }
-      }
-      @media (min-width: ${ScreenSM}) {
-        .block_title {
-          height: 6rem;
-          font-size: 2rem;
-          padding: 1.5rem;
-        }
-        .main_title {
-          font-size: 3rem;
-          margin-bottom: 1rem;
-        }
-      }
-      @media (min-width: ${ScreenXXL}) {
-        .block_title {
-          height: 8rem;
-          font-size: 2.5rem;
-          padding: 2rem;
-        }
-        .main_title {
-          font-size: 4rem;
-          margin-bottom: 2rem;
-        }
-      }
-    `}</style>
   </div>
 );
 
--- components/slides/ProposedMethodology.js
+++ components/slides/ProposedMethodology.js
@@ -1,102 +1,80 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import { Parallax } from 'react-spring';
 
-import { ScreenSM, ScreenXXL } from '../size';
+import { media } from '../size';
 import { pinkColorDark, pinkColorLight } from '../color';
 
 const BackgroundImage = '/static/background_image.jpg';
 
+const Background = styled.div`
+  background-color: ${pinkColorLight};
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  z-index: -100;
+`;
+
+const MainTitle = styled.div`
+  color: ${pinkColorDark};
+  font-style: italic;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+
+  ${media.desktop`
+    font-size: 4rem;
+    margin-bottom: 2rem;
+  `};
+`;
+
+const BlockTitle = styled.div`
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url(${BackgroundImage});
+  background-size: cover;
+  z-index: -100;
+  color: ${pinkColorLight};
+  font-size: 2rem;
+  width: 100%;
+  height: 6rem;
+  margin: 1rem 0;
+  padding: 1.5rem;
+
+  ${media.desktop`
+    font-size: 2.5rem;
+    height: 8rem;
+    padding: 2rem;
+  `};
+`;
+
 const ProposedMethodology = ({ mainOffset, scroll }) => (
   <div onClick={() => scroll(4)}>
     <Parallax.Layer offset={mainOffset} speed={0}>
-      <div className="back_layer" />
+      <Background />
     </Parallax.Layer>
     <Parallax.Layer offset={mainOffset + 0.18} speed={0.5}>
-      <div className="proposedMethodology">
-        <Row className="title_block">
-          <Col span={15} offset={4}>
-            <p className="main_title">Proposed Methodology</p>
-          </Col>
-        </Row>
-        <Row className="block">
-          <Col span={15} offset={4}>
-            <div className="block_title">
-              Bridge the Gap between Simulation and Reality
-            </div>
-          </Col>
-        </Row>
-        <Row className="block">
-          <Col span={15} offset={4}>
-            <div className="block_title">Training in Simulated Enviorments</div>
-          </Col>
-        </Row>
-        <Row className="block">
-          <Col span={15} offset={4}>
-            <div className="block_title">
-              Visual Guidance Module and Target Switching
-            </div>
-          </Col>
-        </Row>
-      </div>
+      <Row>
+        <Col span={15} offset={4}>
+          <MainTitle>Proposed Methodology</MainTitle>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={15} offset={4}>
+          <BlockTitle>Bridge the Gap between Simulation and Reality</BlockTitle>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={15} offset={4}>
+          <BlockTitle>Training in Simulated Enviorments</BlockTitle>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={15} offset={4}>
+          <BlockTitle>Visual Guidance Module and Target Switching</BlockTitle>
+        </Col>
+      </Row>
     </Parallax.Layer>
-    <style jsx>{`
-      .back_layer {
-        background-color: ${pinkColorLight};
-        background-size: cover;
-        width: 100%;
-        height: 100%;
-        z-index: -100;
-      }
-      .main_title {
-        color: ${pinkColorDark};
-        font-style: italic;
-      }
-      .block_title {
-        background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-          url(${BackgroundImage});
-        background-size: cover;
-        color: ${pinkColorLight};
-        width: 100%;
-        z-index: -100;
-        margin: 1rem 0;
-      }
-      @media (max-width: ${ScreenSM}) {
-        // FIXME: 設計要討論
-        .block_title {
-          height: 6rem;
-          font-size: 2rem;
-          padding: 1.5rem;
-        }
-        .main_title {
-          font-size: 3rem;
-          margin-bottom: 1rem;
-        }
-      }
-      @media (min-width: ${ScreenSM}) {
-        .block_title {
-          height: 6rem;
-          font-size: 2rem;
-          padding: 1.5rem;
-        }
-        .main_title {
-          font-size: 3rem;
-          margin-bottom: 1rem;
-        }
-      }
-      @media (min-width: ${ScreenXXL}) {
-        .block_title {
-          height: 8rem;
-          font-size: 2.5rem;
-          padding: 2rem;
-        }
-        .main_title {
-          font-size: 4rem;
-          margin-bottom: 2rem;
-        }
-      }
-    `}</style>
   </div>
 );
 
