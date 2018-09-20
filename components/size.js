30ea1dd5980f212ae518ec469dfff96797c01d4a
terry623 <terry92516@gmail.com>
2018-09-20 23:01:51 +0800

    change topic page to style component

--- components/size.js
+++ components/size.js
@@ -1,5 +1,19 @@
-export const ScreenXXL = '1600px';
-export const ScreenXL = '1200px';
-export const ScreenLG = '992px';
-export const ScreenMD = '768px';
-export const ScreenSM = '576px';
+import { css } from 'styled-components';
+
+const sizes = {
+  desktop: 1600,
+  notebook: 1200,
+};
+
+// TODO: 要搞懂
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label]}px) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
+
+export { media };
--- components/slides/Topic.js
+++ components/slides/Topic.js
@@ -3,97 +3,94 @@ import React from 'react';
 import styled from 'styled-components';
 import { Parallax } from 'react-spring';
 
-import { ScreenSM, ScreenXXL } from '../size';
+import { media } from '../size';
 
 import Header from './Header';
 
 const BackgroundImage = '/static/background_image.jpg';
 
-const Title = styled.h1`
+const BackGround = styled.div`
+  background: url(${BackgroundImage});
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -100;
+`;
+
+const Card = styled.div`
+  width: 70%;
+  height: 60%;
+  background-color: black;
+  opacity: 0.5;
+  border: 1rem solid white;
+  margin: auto;
+  padding: 1rem;
+  border-bottom-width: 7rem;
+
+  ${media.notebook`
+    width: 50%;
+    margin-left: 45%;
+    border-bottom-width: 10rem;
+  `};
+  ${media.desktop`
+    width: 50%;
+    margin-left: 45%;
+    border-bottom-width: 14rem;
+  `};
+`;
+
+const Block = styled.div`
+  padding: 0rem 1rem;
   color: white;
-  font-size: 50px;
+  font-size: 1.2rem;
+
+  ${media.notebook`
+    font-size: 2rem;
+  `};
+  ${media.desktop`
+    font-size: 3rem;
+  `};
+`;
+
+const Year = styled.div`
+  color: white;
+  margin-bottom: 0rem;
+
+  ${media.notebook`
+    margin-bottom: -1rem;
+  `};
+  ${media.desktop`
+    margin-bottom: -1rem;
+  `};
+`;
+
+const Title = styled.div`
+  font-size: 1.5em;
+  font-weight: bold;
 `;
 
 const Topic = ({ mainOffset, scroll }) => (
   <div onClick={() => scroll(1)}>
     <Parallax.Layer offset={mainOffset} speed={0}>
       <Header />
-      <div className="back_layer" />
+      <BackGround />
     </Parallax.Layer>
     <Parallax.Layer offset={mainOffset + 0.3} speed={0.5}>
-      <Title>My page</Title>
-      <div className="card">
-        <div className="title_block">
-          <div className="year_title">2017</div>
-          <div className="main_title">Virtual-to-Real:</div>
-          <div className="sub_title">
+      <Card>
+        <Block>
+          <Year>2017</Year>
+          <Title>Virtual-to-Real:</Title>
+          <div>
             Learning to Control in Visual Semantic
+            <br />
+            Segmentation
           </div>
-          <div className="sub_title">Segmentation</div>
-        </div>
-      </div>
+        </Block>
+      </Card>
     </Parallax.Layer>
-    <style jsx>{`
-      .card {
-        width: 50%;
-        height: 60%;
-        margin-left: 45%;
-        background-color: black;
-        opacity: 0.5;
-        border: 1rem solid white;
-        padding: 1rem;
-      }
-      .title_block {
-        padding: 0rem 1rem;
-        color: white;
-      }
-      .year_title {
-        margin-bottom: -1rem;
-      }
-      .main_title {
-        font-size: 1.5em;
-        font-weight: bold;
-      }
-      @media (max-width: ${ScreenSM}) {
-        .card {
-          margin: auto;
-          width: 70%;
-          border-bottom-width: 6rem;
-        }
-        .title_block {
-          font-size: 1.2rem;
-        }
-        .year_title {
-          margin-bottom: 0rem;
-        }
-      }
-      @media (min-width: ${ScreenSM}) {
-        .card {
-          border-bottom-width: 10rem;
-        }
-        .title_block {
-          font-size: 2rem;
-        }
-      }
-      @media (min-width: ${ScreenXXL}) {
-        .card {
-          border-bottom-width: 14rem;
-        }
-        .title_block {
-          font-size: 3rem;
-        }
-      }
-      .back_layer {
-        background: url(${BackgroundImage});
-        background-size: cover;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -100;
-      }
-    `}</style>
   </div>
 );
 
