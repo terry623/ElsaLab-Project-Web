97dd944980d9733586c37e9f16355c8b10dbe7a7
terry623 <terry92516@gmail.com>
2018-09-30 11:34:58 +0800

    add modal show

--- components/slides/ProposedMethodology.js
+++ components/slides/ProposedMethodology.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import { Parallax } from 'react-spring';
 
 import { pinkColorDark, pinkColorLight } from '../color';
+
+import ResultsModal from './ResultsModal';
 
 const BackgroundImage = '/static/background_image.jpg';
 
@@ -24,6 +26,7 @@ const MainTitle = styled.div`
 `;
 
 const BlockTitle = styled.div`
+  cursor: pointer;
   background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
     url(${BackgroundImage});
   background-size: cover;
@@ -44,42 +47,83 @@ const Square = styled.div`
   margin-top: 3vmin;
 `;
 
-const ProposedMethodology = ({ mainOffset, scroll }) => (
-  <div onClick={() => scroll(4)}>
-    <Parallax.Layer offset={mainOffset} speed={0}>
-      <Background />
-    </Parallax.Layer>
-    <Parallax.Layer offset={mainOffset + 0.18} speed={0.3}>
-      <Row>
-        <Col span={15} offset={4}>
-          <MainTitle>
-            Proposed Methodology
-            <Square />
-          </MainTitle>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={15} offset={4}>
-          <BlockTitle>Bridge the Gap between Simulation and Reality</BlockTitle>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={15} offset={4}>
-          <BlockTitle>Training in Simulated Enviorments</BlockTitle>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={15} offset={4}>
-          <BlockTitle>Visual Guidance Module and Target Switching</BlockTitle>
-        </Col>
-      </Row>
-    </Parallax.Layer>
-  </div>
-);
+const allTitles = [
+  'Bridge the Gap between Simulation and Reality',
+  'Training in Simulated Enviorments',
+  'Visual Guidance Module and Target Switching',
+];
+
+class ProposedMethodology extends Component {
+  state = {
+    modalVisible: false,
+    modalTitle: '',
+  };
+
+  openModal = title => {
+    this.setState({
+      modalVisible: true,
+      modalTitle: title,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalVisible: false,
+    });
+  };
+
+  render() {
+    const { mainOffset } = this.props;
+    const { modalVisible, modalTitle } = this.state;
+
+    return (
+      <div>
+        <Parallax.Layer offset={mainOffset} speed={0}>
+          <Background />
+          <ResultsModal
+            visible={modalVisible}
+            title={modalTitle}
+            closeModal={this.closeModal}
+          />
+        </Parallax.Layer>
+        <Parallax.Layer offset={mainOffset + 0.18} speed={0.3}>
+          <Row>
+            <Col span={15} offset={4}>
+              <MainTitle>
+                Proposed Methodology
+                <Square />
+              </MainTitle>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={15} offset={4}>
+              <BlockTitle onClick={() => this.openModal(allTitles[0])}>
+                {allTitles[0]}
+              </BlockTitle>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={15} offset={4}>
+              <BlockTitle onClick={() => this.openModal(allTitles[1])}>
+                {allTitles[1]}
+              </BlockTitle>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={15} offset={4}>
+              <BlockTitle onClick={() => this.openModal(allTitles[2])}>
+                {allTitles[2]}
+              </BlockTitle>
+            </Col>
+          </Row>
+        </Parallax.Layer>
+      </div>
+    );
+  }
+}
 
 ProposedMethodology.propTypes = {
   mainOffset: PropTypes.number.isRequired,
-  scroll: PropTypes.func.isRequired,
 };
 
 export default ProposedMethodology;
