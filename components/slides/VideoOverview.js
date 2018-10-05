d6abcb54d8d25709e41228ad747374f8e97a571e
terry623 <terry92516@gmail.com>
2018-10-05 16:45:00 +0800

    fix scroll on video

--- components/slides/VideoOverview.js
+++ components/slides/VideoOverview.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import YouTube from 'react-youtube';
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
 import styled from 'styled-components';
 import { Col, Row } from 'antd';
 
 import { pinkColorMid } from '../color';
 
-const videoId = '8osw3ElPAvY';
-
-const opts = {
-  frameBorder: '0',
-  allowFullScreen: true,
-  playerVars: {
-    rel: 0,
-    showinfo: 0,
-    autoplay: 1,
-  },
-};
+const VideoURL =
+  'https://www.youtube.com/embed/8osw3ElPAvY?rel=0&controls=0showinfo=0';
 
 const BackgroundInvertVerticalVerticalImage =
   '/static/background_image_invert_vertical_2.jpg';
+
+const videoConfig = {
+  youtube: {
+    frameBorder: '0',
+    allowFullScreen: true,
+    playerVars: {
+      rel: 0,
+      showinfo: 0,
+    },
+  },
+};
 
 const Background = styled.div`
   background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
@@ -48,11 +50,6 @@ const Title = styled.div`
   padding: 3vmin;
 `;
 
-const YoutubePlayer = styled(YouTube)`
-  width: 100%;
-  height: 100%;
-`;
-
 const Square = styled.div`
   width: 5vmin;
   height: 5vmin;
@@ -61,30 +58,74 @@ const Square = styled.div`
   margin-left: 2vmin;
 `;
 
+const videoSize = {
+  width: '115vmin',
+  height: '100%',
+};
+
 const Wrapper = styled.div`
-  width: 115vmin;
-  height: 100%;
+  position: relative;
+  ${videoSize};
 `;
 
-// FIXME: 滑鼠在影片上，會不能滑動
-const VideoOverview = () => (
-  <div className="section">
-    <Background>
-      <Row type="flex" justify="center">
-        <SystemCol>
-          <MediaTitleBlock>
-            <Title>Video Overview</Title>
-            <Square />
-          </MediaTitleBlock>
-        </SystemCol>
-        <SystemCol>
-          <Wrapper>
-            <YoutubePlayer videoId={videoId} opts={opts} />
-          </Wrapper>
-        </SystemCol>
-      </Row>
-    </Background>
-  </div>
-);
+const TransparentLayer = styled.div`
+  background-color: rgba(255, 255, 255, 0);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+`;
+
+class VideoOverview extends Component {
+  state = {
+    isPlaying: false,
+  };
+
+  playVideo = () => {
+    this.setState({
+      isPlaying: true,
+    });
+  };
+
+  onPause = () => {
+    this.setState({ isPlaying: false });
+  };
+
+  render() {
+    const { isPlaying } = this.state;
+
+    return (
+      <div className="section">
+        <Background>
+          <Row type="flex" justify="center">
+            <SystemCol>
+              <MediaTitleBlock>
+                <Title>Video Overview</Title>
+                <Square />
+              </MediaTitleBlock>
+            </SystemCol>
+            <SystemCol>
+              <Wrapper>
+                <ReactPlayer
+                  url={VideoURL}
+                  playing={isPlaying}
+                  width={videoSize.width}
+                  height={videoSize.height}
+                  config={videoConfig}
+                  onPause={this.onPause}
+                />
+                {!isPlaying ? (
+                  <TransparentLayer onClick={this.playVideo} />
+                ) : null}
+              </Wrapper>
+            </SystemCol>
+          </Row>
+        </Background>
+      </div>
+    );
+  }
+}
 
 export default VideoOverview;
