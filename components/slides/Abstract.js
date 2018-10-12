d79108f3c8d39a2531c0c8d196b88cf8f0158faa
terry623 <terry92516@gmail.com>
2018-10-12 15:12:01 +0800

    change abstract to rwd

--- components/slides/Abstract.js
+++ components/slides/Abstract.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Col, Row } from 'antd';
 
+import { media } from '../size';
 import { pinkColorLight } from '../color';
 
 const BackgroundInvertVerticalImage =
@@ -23,64 +24,77 @@ const Content = styled.p`
 
 const Title = styled.div`
   color: ${pinkColorLight};
-  font-size: 6vmin;
+  font-size: 7vmin;
   margin-top: 18vmin;
   margin-bottom: 2vmin;
+
+  ${media.lessThan('notebook')`
+    margin-top: 2vmin;
+  `};
 `;
 
 const Square = styled.div`
   width: 5vmin;
   height: 5vmin;
+  float: right;
+  margin-top: 2vmin;
+  margin-right: 2vmin;
   background-color: ${pinkColorLight};
-  margin-bottom: 1.5vmin;
 `;
 
 const Bar = styled.div`
   width: 1.5vmin;
   height: 72vmin;
   background-color: ${pinkColorLight};
-  float: right;
-  margin-right: 3vmin;
+  position: absolute;
+  margin-left: 28vmin;
+  margin-top: 28vmin;
+
+  ${media.lessThan('notebook')`
+    position: relative;
+    float:left;
+    margin-left: 0;
+    margin-top:20vmin;
+    width: 40vmin;
+    height: 1.5vmin;
+  `};
 `;
 
 const Abstract = () => (
   <div className="section">
     <Background>
+      <Bar />
       <Row>
-        <Col span={12} offset={4}>
-          <Title>
-            <Row type="flex" justify="space-between" align="bottom">
-              <Col span={2}>Abstract</Col>
-              <Col span={2}>
+        <Col xs={{ span: 16, offset: 4 }} xl={{ span: 12, offset: 4 }}>
+          <Row>
+            <Title>
+              <Col span={12}>Abstract</Col>
+              <Col span={12}>
                 <Square />
               </Col>
-            </Row>
-          </Title>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={1} offset={3}>
-          <Bar />
-        </Col>
-        <Col span={12} offset={0}>
-          <Content>
-            Collecting training data from the physical world is usually
-            time-consuming and even dangerous for fragile robots, and therefore,
-            recent advances in robot learning propose to use simulators as the
-            training platform. Unfortunately, the reality gap between synthetic
-            and real visual data prohibits directly migrating the models trained
-            in virtual worlds to the real world. This paper proposes a modular
-            architecture for tackling simulation- to-reality problem. The
-            architecture separates the learning model into a perception module
-            and a control policy module, and uses semantic image segmentation as
-            the meta representation for relating these two modules. The
-            perception module translates RGB images to semantic image
-            segmentations. The control policy module employs deep reinforcement
-            learning (RL) methods and takes image segmentations as its inputs.
-            Experimental results show that our architecture outperforms all
-            baseline methods in the virtual and real environments, and learns
-            faster than them.
-          </Content>
+            </Title>
+            <Col span={24}>
+              <Content>
+                Collecting training data from the physical world is usually
+                time-consuming and even dangerous for fragile robots, and
+                therefore, recent advances in robot learning propose to use
+                simulators as the training platform. Unfortunately, the reality
+                gap between synthetic and real visual data prohibits directly
+                migrating the models trained in virtual worlds to the real
+                world. This paper proposes a modular architecture for tackling
+                simulation- to-reality problem. The architecture separates the
+                learning model into a perception module and a control policy
+                module, and uses semantic image segmentation as the meta
+                representation for relating these two modules. The perception
+                module translates RGB images to semantic image segmentations.
+                The control policy module employs deep reinforcement learning
+                (RL) methods and takes image segmentations as its inputs.
+                Experimental results show that our architecture outperforms all
+                baseline methods in the virtual and real environments, and
+                learns faster than them.
+              </Content>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Background>
