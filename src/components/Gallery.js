2dfc69debb341ed678c11c1321c164f0411e8cfc
terry623 <terry92516@gmail.com>
2018-10-28 21:17:40 +0800

    add header color ( not yet )

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import { Link } from 'react-router-dom';
 
 import GalleryBackgroundImage from './static/gallery_background_image.jpg';
 import Header from './Header';
+import { pinkColorDark, transparentBlack } from './color';
 
 const Background = styled.div`
   background: url(${GalleryBackgroundImage});
@@ -24,6 +25,7 @@ const Card = styled.div`
 `;
 
 const Title = styled.div`
+  color: ${transparentBlack};
   font-size: 5vmin;
   font-weight: bold;
   font-style: italic;
@@ -31,41 +33,57 @@ const Title = styled.div`
   margin-bottom: 2vmin;
 `;
 
-const Topic = () => (
-  <div className="section">
-    <Background>
-      <Header />
-      <Row type="flex" justify="start">
-        <Col span={12} offset={3}>
-          <Title>Recent Project</Title>
-        </Col>
-      </Row>
-      <Row type="flex" justify="center" gutter={32}>
-        <Col span={6}>
-          <Link to="/project/Virtual-to-Real">
-            <Card>Click Here !!</Card>
-          </Link>
-        </Col>
-        <Col span={6}>
-          <Card />
-        </Col>
-        <Col span={6}>
-          <Card />
-        </Col>
-      </Row>
-      <Row type="flex" justify="center" gutter={32}>
-        <Col span={6}>
-          <Card />
-        </Col>
-        <Col span={6}>
-          <Card />
-        </Col>
-        <Col span={6}>
-          <Card />
-        </Col>
-      </Row>
-    </Background>
-  </div>
-);
+const fontColorMap = {
+  'Virtual-to-Real': pinkColorDark,
+  'Dynamic-Video': transparentBlack,
+};
 
-export default Topic;
+class Gallery extends Component {
+  state = {
+    project: 'Dynamic-Video',
+  };
+
+  render() {
+    const { project } = this.state;
+    const color = fontColorMap[project];
+
+    return (
+      <div className="section">
+        <Background>
+          <Header color={color} />
+          <Row type="flex" justify="start">
+            <Col span={12} offset={3}>
+              <Title>Recent Project</Title>
+            </Col>
+          </Row>
+          <Row type="flex" justify="center" gutter={32}>
+            <Col span={6}>
+              <Link to="/project/Virtual-to-Real">
+                <Card>Click Here !!</Card>
+              </Link>
+            </Col>
+            <Col span={6}>
+              <Card />
+            </Col>
+            <Col span={6}>
+              <Card />
+            </Col>
+          </Row>
+          <Row type="flex" justify="center" gutter={32}>
+            <Col span={6}>
+              <Card />
+            </Col>
+            <Col span={6}>
+              <Card />
+            </Col>
+            <Col span={6}>
+              <Card />
+            </Col>
+          </Row>
+        </Background>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
--- src/components/color.js
+++ src/components/color.js
@@ -1,3 +1,4 @@
 export const pinkColorLight = '#fddde4';
 export const pinkColorMid = '#ffc7dc';
 export const pinkColorDark = '#ff75a8';
+export const transparentBlack = 'rgba(0, 0, 0, 0.6)';
